69304b4c06eac76c998a5160d5855e3ad39cc35f
zaibisamad <152690452+zaibisamad@users.noreply.github.com>
2023-12-07 20:02:31 +0500

    Add files via upload

--- Program.vb
+++ Program.vb
@@ -1,41 +1,30 @@
 Imports System
-Imports System.Xml
 
 Module Program
     Sub Main()
-        'swapping first two characters of the 2 words in a string
 
-        Dim str1, str2 As String
-        Dim x, y, a, b, p, q As String
-        Dim len1 As Integer
-        Dim len2 As Integer
+        Dim STR1, STR2, Char1, Char2, Char3 As String
+        Dim COUNT As Integer
+        STR1 = ""
+        STR2 = ""
 
-        len1 = 0
-        len2 = 0
-        a = ""
-        b = ""
-        
-        Console.Write("enter a word: ")
-        str1 = Console.ReadLine()
-        Console.Write("enter another word: ")
-        str2 = Console.ReadLine()
+        Console.Write("Enter a string: ")
+        STR1 = Console.ReadLine()
+        Console.Write("What to replace: ")
+        Char1 = Console.ReadLine()
+        Console.Write("Enter character to replace with: ")
+        Char2 = Console.ReadLine
 
-        len1 = Len(str1)
-        len2 = Len(str2)
-
-        x = Left(str1, 2) 'string1 ke 2 chars
-        y = Left(str2, 2) 'string2 ke 2 chars
-        a = Right(str1, len1 - 2) 'remaining chars of string1
-        b = Right(str2, len2 - 2)  'remaining chars of string2
-        p = x & b
-        q = y & a
-
-
-        If len2 > 2 And len1 > 2 Then
-            Console.WriteLine("result:" & p & " " & q)
-        Else
-            Console.WriteLine("enter longer word nexxt time. ")
-        End If
+        For COUNT = 1 To Len(STR1)
+            Char3 = Mid(STR1, COUNT, 1)
+            If Char1 <> Char3 Then
+                STR2 = STR2 & Char3
+            Else
+                STR2 = STR2 & Char2
+            End If
+        Next COUNT
+        Console.WriteLine(STR2)
         Console.ReadKey()
+
     End Sub
 End Module
